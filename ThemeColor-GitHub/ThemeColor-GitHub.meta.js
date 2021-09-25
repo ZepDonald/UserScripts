44de42e11f7bf5a4a944e98c366986977a3bea8a
ZepDonald <marcel-github@von-au.net>
2021-09-25 18:10:43 +0200

    split up user and meta for github

---
+++ ThemeColor-GitHub/ThemeColor-GitHub.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Theme Color: GitHub
+// @description Set Theme Color for Github on Safari 15+
+// @version     1.0.0
+// @updateURL   https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-GitHub/ThemeColor-GitHub.meta.js
+// @downloadURL https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-GitHub/ThemeColor-GitHub.user.js
+// @match       https://github.com/*
+// @include     https://github.com/*
+// ==/UserScript==
