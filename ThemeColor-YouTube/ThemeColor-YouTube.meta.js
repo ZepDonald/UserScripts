ee4bff6058cb9adcccdaee15046ae2e5ff4d3e9e
ZepDonald <marcel-github@von-au.net>
2021-09-25 18:12:00 +0200

    split up user and meta for youtube

---
+++ ThemeColor-YouTube/ThemeColor-YouTube.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Theme Color: YouTube
+// @description Set Theme Color for YouTube on Safari 15+
+// @version     1.0.0
+// @updateURL   https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-YouTube/ThemeColor-YouTube.meta.js
+// @downloadURL https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-YouTube/ThemeColor-YouTube.user.js
+// @match       https://www.youtube.com/*
+// @include     https://www.youtube.com/*
+// ==/UserScript==
