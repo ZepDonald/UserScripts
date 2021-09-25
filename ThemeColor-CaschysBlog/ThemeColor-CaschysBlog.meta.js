4166ed4a02104f737131daa6ade4263efbac49b2
ZepDonald <marcel-github@von-au.net>
2021-09-25 18:07:59 +0200

    split up meta and user

---
+++ ThemeColor-CaschysBlog/ThemeColor-CaschysBlog.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Theme Color: Caschys Blog
+// @description Set Theme Color for Caschys Blog on Safari 15+
+// @version     1.0.0
+// @updateURL   https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-CaschysBlog/ThemeColor-CaschysBlog.meta.js
+// @downloadURL https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-CaschysBlog/ThemeColor-CaschysBlog.user.js
+// @match       https://stadt-bremerhaven.de/*
+// @include     https://stadt-bremerhaven.de/*
+// ==/UserScript==
