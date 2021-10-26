d3f8725ab6e2dde7a32f90fba01ba71dee592b09
ZepDonald <marcel-github@von-au.net>
2021-10-26 15:09:14 +0200

    Added Theme Color Startpage.com

---
+++ ThemeColor-StartpageCom/ThemeColor-StartpageCom.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Theme Color: Startpage.com
+// @description Set Theme Color for Startpage.com on Safari 15+
+// @version     1.0.0
+// @ updateURL   https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-StartpageCom/ThemeColor-StartpageCom.meta.js
+// @ downloadURL https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-StartpageCom/ThemeColor-StartpageCom.user.js
+// @match       https://www.startpage.com/*
+// @include     https://www.startpage.com/*
+// ==/UserScript==
---
+++ ThemeColor-StartpageCom/ThemeColor-StartpageCom.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Theme Color: Startpage.com
+// @description Set Theme Color for Startpage.com on Safari 15+
+// @version     1.0.0
+// @ updateURL   https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-StartpageCom/ThemeColor-StartpageCom.meta.js
+// @ downloadURL https://github.com/ZepDonald/UserScripts/raw/main/ThemeColor-StartpageCom/ThemeColor-StartpageCom.user.js
+// @match       https://www.startpage.com/*
+// @include     https://www.startpage.com/*
+// ==/UserScript==
+
+(function() {
+    var meta = document.createElement('meta');
+    meta.name = "theme-color";
+    meta.content = "#2A3761";
+    document.getElementsByTagName('head')[0].appendChild(meta);
+})();
